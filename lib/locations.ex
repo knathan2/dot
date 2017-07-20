535da8593dd90169d484a6c06b5445af53c17a75
Nathan Kendrick <knathan2@vt.edu>
2017-07-20 19:58:56 -0400

    logger

--- lib/locations.ex
+++ lib/locations.ex
@@ -1,5 +1,7 @@
 defmodule Locations do 
+  require Logger
   def loc(place) do
+    Logger.info("#{inspect place}")
     key = String.to_atom(place)
     map = %{:"collegiate" => "P.H.D", :"the movies" => "M.S.S", :"the grocery store" => "M.S.S", :"the math emporium" => "U.C.B", :"maple ridge" => "M.S.N", :"pheasant run" => "P.H.D", :"terrace view" => "P.H.D", :"fox ridge" => "H.W.D", :"the village" => "P.R.O"}
     map[key]
