3f6c58ee296103e7028a4d9c3d4ccc64d25c5d55
Nathan Kendrick <knathan2@vt.edu>
2017-07-20 10:24:31 -0400

    try fox ridge again

--- lib/locations.ex
+++ lib/locations.ex
@@ -1,7 +1,7 @@
 defmodule Locations do 
   def loc(place) do
     key = String.to_atom(place)
-    map = %{:"collegiate" => "P.H.D", :"the movies" => "M.S.S", :"the grocery store" => "M.S.S", :"the math emporium" => "U.C.B", :"maple ridge" => "M.S.N", :"pheasant run" => "P.H.D", :"terrace view" => "P.H.D", :"fridge" => "H.W.D", :"the village" => "P.R.O"}
+    map = %{:"collegiate" => "P.H.D", :"the movies" => "M.S.S", :"the grocery store" => "M.S.S", :"the math emporium" => "U.C.B", :"maple ridge" => "M.S.N", :"pheasant run" => "P.H.D", :"terrace view" => "P.H.D", :"fox ridge" => "H.W.D", :"the village" => "P.R.O"}
     map[key]
   end
 end
